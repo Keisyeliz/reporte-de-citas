e1b3f85e48f285be8b3397ab543673d807198e3f
Keisyeliz <158522128+Keisyeliz@users.noreply.github.com>
2024-03-19 16:24:45 -0500

    Update registro_citas.py

--- registro_citas.py
+++ registro_citas.py
@@ -1,43 +1,45 @@
 import os
-    
+
 odontologia = 0
 ginecologia = 0
 consulta_general = 0
 maternidad = 0
-infancia = 0
-repetir = True 
+infantes = 0
+repetir = True
 
-def fnt_limpiarPantalla():
-        os.system('cls')
-        
+def fnt_limpiarpantalla():
+    os.system('cls')
+
 def fnt_opcion(op):
-    global odontologia,consulta_general,maternidad,infancia
+    global odontologia, ginecologia, consulta_general, maternidad, infantes
     if (op == 1):
-         odontologia += 1
-        
+        odontologia += 1
     if (op == 2):
         ginecologia += 1
-            
     if (op == 3):
         consulta_general += 1
-            
     if (op == 4):
         maternidad += 1
-            
-    if  (op == 5):
-        infancia += 1
-        
+    if (op == 5):
+        infantes += 1
+    
+    
+def fnt_reporte():
+    fnt_limpiarpantalla()
+    global odontologia, ginecologia, consulta_general, maternidad, infantes
+    print('Odontologia: ',odontologia)
+    print('Ginecologia: ',ginecologia)
+    print('Consulta general: ',consulta_general)
+    print('Maternidad: ',maternidad)
+    print('infantes: ' ,infantes)
 
-    def fnt_reporte():
-        global odontologia,ginecologia,consulta_general,maternidad,infancia
-        print("Odontología: ",odontologia)
-        print("Ginecología: ",ginecologia)
-        print("Consulta general: ",consulta_general)
-        print("Maternidad: ",maternidad)
-        print("Infancia: ",infancia)
-
-    while repetir == True:
-        fnt_limpiarPantalla()
-        opcion = int(input('---MENÚ DE OPCIONES---\n1. Odontologia\n2. Ginecologia\n3. Consulta general\n4. Maternidad\n5. Infancia\n6. Salir\n -> '))
-        fnt_opcion(opcion)
-        enter = input('\n <ENTER>')
+while repetir == True:
+    fnt_limpiarpantalla()
+    opcion = int(input('1. Odontologia\n2. Ginecologia\n3. Consulta general \n4. Maternidad\n5. infantes\n6. Salir\n7. Reporte\n -> '))
+    fnt_opcion(opcion)
+    if (opcion == 6):
+        repetir = False
+    else:
+        if (opcion == 7):
+            fnt_reporte()
+            enter = input('\n<ENTER>')
